3b8777741fe11ec36c11c96b4a7733655bbb9da5
Rodrigo Borges <rasb140@hotmail.com>
2022-12-27 15:28:41 -0100

    Fixing Paths, Database and Cloudinary, 2nd phase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_104355) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_172859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,31 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_104355) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "directors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.text "synopsis"
+    t.bigint "director_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["director_id"], name: "index_movies_on_director_id"
+  end
+
+  create_table "tv_shows", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.text "synopsis"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -91,4 +116,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_104355) do
   add_foreign_key "airplanes", "users"
   add_foreign_key "bookings", "airplanes"
   add_foreign_key "bookings", "users"
+  add_foreign_key "movies", "directors"
 end
