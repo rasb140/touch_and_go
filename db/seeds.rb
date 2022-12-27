65e7170561fb3263de56474510ecc1b601ce6404
Rodrigo Borges <rasb140@hotmail.com>
2022-12-27 16:18:14 -0100

    Aircraft images correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,36 +15,36 @@ puts "Kevin is created"
 
 plane = Airplane.new({ brand_and_model: "GULFSTREAM G700", weight: 48_806, registration: "N700GS", range: 7_500, speed: "MACH 0.90", max_altitude: 51_000, number_of_engines: 2, pax_capacity: 10, address: "Red Square, Moscow" })
 plane.user = kevin
-plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'plane.jpg')), filename: 'plane.jpg', content_type: 'image/jpeg')
+plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'gulfstream G700.jpg')), filename: 'gulfstream G700.jpg', content_type: 'image/jpeg')
 plane.save
 puts "GULFSTREAM G700 is created"
 
 plane = Airplane.new({ brand_and_model: "DASSAULT FALCON 8X", weight: 33_112, registration: "CS-TCR", range: 6_450, speed: "MACH 0.90", max_altitude: 51_000, number_of_engines: 3, pax_capacity: 15, address: "Le tour Effel, Paris" })
 plane.user = kevin
-plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'plane.jpg')), filename: 'plane.jpg', content_type: 'image/jpeg')
+plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'DASSAULT FALCON 8X.png')), filename: 'DASSAULT FALCON 8X.png', content_type: 'image/jpeg')
 plane.save
 puts "DASSAULT FALCON 8X is created"
 
 plane = Airplane.new({ brand_and_model: "CESSNA 152", weight: 750, registration: "CS-ASP", range: 768, speed: 107, max_altitude: 14_700, number_of_engines: 1, pax_capacity: 1, address: "Branderburger Tor, Berlin" })
 plane.user = kevin
-plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'plane.jpg')), filename: 'plane.jpg', content_type: 'image/jpeg')
+plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'Cessna-152-CS-ASP.jpeg')), filename: 'Cessna-152-CS-ASP.jpeg', content_type: 'image/jpeg')
 plane.save
 puts "CESSNA 152 is created"
 
 plane = Airplane.new({ brand_and_model: "BEECHCRAFT KING AIR 350I", weight: 6_804, registration: "F-KMJG", range: 1806, speed: 312, max_altitude: 35_000, number_of_engines: 2, pax_capacity: 11, address: "Place Kleber, Strassburg" })
 plane.user = kevin
-plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'plane.jpg')), filename: 'plane.jpg', content_type: 'image/jpeg')
+plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'beechcraft_kingair_350i_photo1.jpg__1200x675_q85_crop_subsampling-2_upscale.jpg')), filename: 'beechcraft_kingair_350i_photo1.jpg__1200x675_q85_crop_subsampling-2_upscale.jpg', content_type: 'image/jpeg')
 plane.save
 puts "BEECHCRAFT KING AIR 350I  is created"
 
 plane = Airplane.new({ brand_and_model: "AIRBUS ACJ 319", weight: 78_200, registration: "G-JFOG", range: 6000, speed: 0.82, max_altitude: 41_000, number_of_engines: 2, pax_capacity: 50, address: "Azores" })
 plane.user = kevin
-plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'plane.jpg')), filename: 'plane.jpg', content_type: 'image/jpeg')
+plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'airbus_acj319neo_side_l_to_r.jpg__1200x675_q85_crop_subsampling-2_upscale.jpg')), filename: 'airbus_acj319neo_side_l_to_r.jpg__1200x675_q85_crop_subsampling-2_upscale.jpg', content_type: 'image/jpeg')
 plane.save
 puts "AIRBUS ACJ 319 is created"
 
 plane = Airplane.new({ brand_and_model: "VELIS ELECTRO", weight: 600, registration: "CS-AEL", range: 75, speed: 90, max_altitude: 12_000, number_of_engines: 1, pax_capacity: 1, address: "New York" })
 plane.user = kevin
-plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'plane.jpg')), filename: 'plane.jpg', content_type: 'image/jpeg')
+plane.photos.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'Pipistrel_Velis_Electro_9992.jpg')), filename: 'Pipistrel_Velis_Electro_9992.jpg', content_type: 'image/jpeg')
 plane.save
 puts "VELIS ELECTRO is created"
